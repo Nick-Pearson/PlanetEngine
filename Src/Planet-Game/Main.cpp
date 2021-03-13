f79f96e3c8b51e8baecb0414371bccb33ac2eeda
Nick Pearson <nicholas.pearson4@btinternet.com>
2021-03-13 13:37:48 +0000

    fix scaling issue

--- Src/Planet-Game/Main.cpp
+++ Src/Planet-Game/Main.cpp
@@ -4,9 +4,11 @@
 
 #if PLATFORM_WIN
 #include "D3DRenderSystem.h"
+#include <shellscalingapi.h>
 
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+    SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
     Window window{1280, 720};
     window.SetWindowName("PlanetEngine");
     D3DRenderSystem* renderSystem = new D3DRenderSystem{window.GetWindowHandle()};
