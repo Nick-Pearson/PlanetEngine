7f1a719bd6e666864b067106f3c5ca5815b2f2ca
Nick Pearson <Nick-Pearson@users.noreply.github.com>
2023-01-02 17:39:29 +0000

    add wxwidgets

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class PlanetEngine(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "spdlog/1.8.2","stb/20200203","imgui/1.88","libpng/1.6.37","openfbx/cci.20210426"
+   requires = "spdlog/1.8.2","stb/20200203","imgui/1.88","libpng/1.6.37","openfbx/cci.20210426","wxwidgets/3.1.5@bincrafters/stable"
    build_requires = "gtest/1.10.0"
    generators = "cmake", "gcc", "txt"
 
