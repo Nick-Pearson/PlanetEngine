37326aac9322c9b83bb6d62fb8ae96dbc4ab1b1f
Nick Pearson <Nick-Pearson@users.noreply.github.com>
2023-05-15 22:18:21 +0100

    window

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,88 @@
+use windows::{
+    core::*,
+    Win32::Foundation::*,
+    Win32::System::LibraryLoader::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
+
+use std::mem::transmute;
+
+fn run_engine() -> Result<()> {
+    let instance = unsafe { GetModuleHandleA(None)? };
+
+    let wc = WNDCLASSEXA {
+        cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
+        style: CS_OWNDC,
+        lpfnWndProc: Some(wndproc),
+        hInstance: instance,
+        lpszClassName: s!("EngWindowClass"),
+        ..Default::default()
+    };
+
+    let atom = unsafe { RegisterClassExA(&wc) };
+    debug_assert_ne!(atom, 0);
+
+    let hwnd = unsafe {
+        CreateWindowExA(
+            WINDOW_EX_STYLE::default(),
+            s!("EngWindowClass"),
+            s!("Engine"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            1280,
+            720,
+            None, // no parent window
+            None, // no menus
+            instance,
+            None,
+        )
+    };
+    unsafe { ShowWindow(hwnd, SW_SHOW) };
+
+    loop {
+        let mut message = MSG::default();
+
+        if unsafe { PeekMessageA(&mut message, None, 0, 0, PM_REMOVE) }.into() {
+            unsafe {
+                TranslateMessage(&message);
+                DispatchMessageA(&message);
+            }
+
+            if message.message == WM_QUIT {
+                break;
+            }
+        }
+    }
+
+    Ok(())
+
+}
+
+extern "system" fn wndproc(
+    window: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    match message {
+        WM_CREATE => {
+            unsafe {
+                let create_struct: &CREATESTRUCTA = transmute(lparam);
+                SetWindowLongPtrA(window, GWLP_USERDATA, create_struct.lpCreateParams as _);
+            }
+            LRESULT::default()
+        }
+        WM_DESTROY => {
+            unsafe { PostQuitMessage(0) };
+            LRESULT::default()
+        }
+        _ => {
+            unsafe { DefWindowProcA(window, message, wparam, lparam) }
+        }
+    }
+}
 
 fn main() {
-    println!("Hello world!");
+    run_engine().unwrap();
 }
