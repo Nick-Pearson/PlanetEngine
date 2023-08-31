b1a615111650209e99e8946f69669f4f9ad7a275
Nick Pearson <Nick-Pearson@users.noreply.github.com>
2023-08-31 22:23:42 +0100

    make d3d a module

---
+++ src/graphics/d3dgraphics/d3drootsignature.rs
@@ -0,0 +1,188 @@
+use crate::material::PixelShader;
+use glam::{Mat4, Vec3A};
+use std::mem::size_of;
+
+use windows::Win32::Graphics::Direct3D12::*;
+
+#[repr(C)]
+pub struct D3DSlowVSConstants {
+    world: Mat4,
+    view: Mat4,
+}
+
+impl D3DSlowVSConstants {
+    pub const SIZE_32_BIT: u32 = (size_of::<Self>() / size_of::<u32>()) as u32;
+}
+
+#[repr(C)]
+pub struct D3DFastConstants {
+    model: Mat4,
+    sun_dir: Vec3A,
+    sun_sky_strength: f32,
+    sun_col: Vec3A,
+}
+
+impl D3DFastConstants {
+    pub const SIZE_32_BIT: u32 = (size_of::<Self>() / size_of::<u32>()) as u32;
+}
+
+pub struct D3DRootSignature {
+    root_signature: ID3D12RootSignature,
+}
+
+impl D3DRootSignature {
+    fn check_root_signature_features(device: &ID3D12Device2) -> D3D12_FEATURE_DATA_ROOT_SIGNATURE {
+        let mut feature_data = D3D12_FEATURE_DATA_ROOT_SIGNATURE {
+            HighestVersion: D3D_ROOT_SIGNATURE_VERSION_1_1,
+        };
+
+        let result = unsafe {
+            let ptr = &mut feature_data as *mut _ as *mut std::ffi::c_void;
+            let size = size_of::<D3D12_FEATURE_DATA_ROOT_SIGNATURE>() as u32;
+            device.CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, ptr, size)
+        };
+        if result.is_err() {
+            feature_data.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+        }
+        return feature_data;
+    }
+
+    fn init_as_constants(
+        num_32bit_values: u32,
+        shader_register: u32,
+        register_space: u32,
+        visibility: D3D12_SHADER_VISIBILITY,
+    ) -> D3D12_ROOT_PARAMETER1 {
+        let constants = D3D12_ROOT_CONSTANTS {
+            ShaderRegister: shader_register,
+            RegisterSpace: register_space,
+            Num32BitValues: num_32bit_values,
+        };
+        return D3D12_ROOT_PARAMETER1 {
+            ParameterType: D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS,
+            Anonymous: D3D12_ROOT_PARAMETER1_0 {
+                Constants: constants,
+            },
+            ShaderVisibility: visibility,
+        };
+    }
+
+    fn init_as_descriptor_table(
+        range_type: D3D12_DESCRIPTOR_RANGE_TYPE,
+        num_descriptors: usize,
+        shader_visibility: D3D12_SHADER_VISIBILITY,
+    ) -> D3D12_ROOT_PARAMETER1 {
+        let ranges = vec![D3D12_DESCRIPTOR_RANGE1 {
+            RangeType: range_type,
+            NumDescriptors: num_descriptors as u32,
+            BaseShaderRegister: 0,
+            RegisterSpace: 0,
+            Flags: D3D12_DESCRIPTOR_RANGE_FLAG_NONE,
+            OffsetInDescriptorsFromTableStart: D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND,
+        }];
+        let descriptor_table = D3D12_ROOT_DESCRIPTOR_TABLE1 {
+            NumDescriptorRanges: 1,
+            pDescriptorRanges: ranges.as_ptr(),
+        };
+        return D3D12_ROOT_PARAMETER1 {
+            ParameterType: D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS,
+            Anonymous: D3D12_ROOT_PARAMETER1_0 {
+                DescriptorTable: descriptor_table,
+            },
+            ShaderVisibility: shader_visibility,
+        };
+    }
+
+    fn init_1_1(
+        params: Vec<D3D12_ROOT_PARAMETER1>,
+        samplers: Vec<D3D12_STATIC_SAMPLER_DESC>,
+        flags: D3D12_ROOT_SIGNATURE_FLAGS,
+    ) -> D3D12_VERSIONED_ROOT_SIGNATURE_DESC {
+        let desc = D3D12_ROOT_SIGNATURE_DESC1 {
+            NumParameters: params.len() as u32,
+            pParameters: params.as_ptr(),
+            NumStaticSamplers: samplers.len() as u32,
+            pStaticSamplers: samplers.as_ptr(),
+            Flags: flags,
+        };
+
+        return D3D12_VERSIONED_ROOT_SIGNATURE_DESC {
+            Version: D3D_ROOT_SIGNATURE_VERSION_1_1,
+            Anonymous: D3D12_VERSIONED_ROOT_SIGNATURE_DESC_0 { Desc_1_1: desc },
+        };
+    }
+
+    pub fn from_pixel_shader(
+        pixel_shader: &PixelShader,
+        device: &ID3D12Device2,
+    ) -> D3DRootSignature {
+        let feature_data = Self::check_root_signature_features(device);
+
+        let mut params: Vec<D3D12_ROOT_PARAMETER1> = Vec::new();
+        params.push(Self::init_as_constants(
+            D3DSlowVSConstants::SIZE_32_BIT,
+            0,
+            0,
+            D3D12_SHADER_VISIBILITY_VERTEX,
+        ));
+        params.push(Self::init_as_constants(
+            D3DFastConstants::SIZE_32_BIT,
+            1,
+            0,
+            D3D12_SHADER_VISIBILITY_ALL,
+        ));
+
+        let num_inputs = pixel_shader.inputs.len();
+        if num_inputs > 0 {
+            params.push(Self::init_as_descriptor_table(
+                D3D12_DESCRIPTOR_RANGE_TYPE_SRV,
+                num_inputs,
+                D3D12_SHADER_VISIBILITY_PIXEL,
+            ));
+        }
+
+        // create a static sampler
+        let sampler = D3D12_STATIC_SAMPLER_DESC {
+            Filter: D3D12_FILTER_MIN_MAG_MIP_POINT,
+            AddressU: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
+            AddressV: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
+            AddressW: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
+            MipLODBias: 0.0,
+            MaxAnisotropy: 0,
+            ComparisonFunc: D3D12_COMPARISON_FUNC_NEVER,
+            BorderColor: D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK,
+            MinLOD: 0.0,
+            MaxLOD: D3D12_FLOAT32_MAX,
+            ShaderRegister: 0,
+            RegisterSpace: 0,
+            ShaderVisibility: D3D12_SHADER_VISIBILITY_PIXEL,
+        };
+
+        let flags: D3D12_ROOT_SIGNATURE_FLAGS =
+            D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT
+                | D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS
+                | D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS
+                | D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS;
+
+        let desc = Self::init_1_1(params, vec![sampler], flags);
+
+        let mut blob_opt = None;
+        let mut error_blob = None;
+        unsafe {
+            D3D12SerializeVersionedRootSignature(&desc, &mut blob_opt, Some(&mut error_blob))
+        }
+        .unwrap();
+        let blob = blob_opt.unwrap();
+        let signature = unsafe {
+            device.CreateRootSignature(
+                0,
+                std::slice::from_raw_parts(blob.GetBufferPointer() as _, blob.GetBufferSize()),
+            )
+        }
+        .unwrap();
+
+        return D3DRootSignature {
+            root_signature: signature,
+        };
+    }
+}
--- src/graphics/d3dgraphics/mod.rs
+++ src/graphics/d3dgraphics/mod.rs
@@ -1,12 +1,18 @@
-use crate::{graphics::*, material::PixelShader};
+mod d3drootsignature;
+
+use crate::graphics::*;
 use arrayvec::ArrayVec;
-use glam::{Mat4, Vec3, Vec3A};
-use std::{cell::Cell, mem::{ManuallyDrop, size_of}};
+use std::{
+    cell::Cell,
+    mem::ManuallyDrop,
+};
 
 use windows::{
     core::*, Win32::Foundation::*, Win32::Graphics::Direct3D::*, Win32::Graphics::Direct3D12::*,
     Win32::Graphics::Dxgi::Common::*, Win32::Graphics::Dxgi::*, Win32::System::Threading::*,
 };
+
+use self::d3drootsignature::D3DRootSignature;
 
 struct D3DCommandQueue {
     next_signal: Cell<u64>,
@@ -476,138 +482,6 @@ impl WindowRenderTarget {
     }
 }
 
-#[repr(C)]
-struct D3DSlowVSConstants {
- world: Mat4,
- view: Mat4,
-}
-
-impl D3DSlowVSConstants {
-    pub const SIZE_32_BIT:u32 = (size_of::<Self>() / size_of::<u32>()) as u32;
-}
-
-#[repr(C)]
-struct D3DFastConstants {
-model: Mat4,
-sun_dir: Vec3A,
-sun_sky_strength: f32,
-sun_col: Vec3A
-}
-
-impl D3DFastConstants {
-    pub const SIZE_32_BIT:u32 = (size_of::<Self>() / size_of::<u32>()) as u32;
-}
-
-pub struct D3DRootSignature {
-    root_signature: ID3D12RootSignature
-}
-
-impl D3DRootSignature {
-    fn check_root_signature_features(device: &ID3D12Device2) -> D3D12_FEATURE_DATA_ROOT_SIGNATURE {
-        let mut feature_data = D3D12_FEATURE_DATA_ROOT_SIGNATURE {
-            HighestVersion: D3D_ROOT_SIGNATURE_VERSION_1_1
-        };
-        
-        let result = unsafe { 
-            let ptr = &mut feature_data as *mut _ as *mut std::ffi::c_void;
-            let size = size_of::<D3D12_FEATURE_DATA_ROOT_SIGNATURE>() as u32;
-            device.CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, ptr, size)
-        };
-        if result.is_err()
-        {
-            feature_data.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
-        }
-        return feature_data;
-    }
-
-    fn init_as_constants(num_32bit_values: u32, 
-        shader_register: u32,
-        register_space: u32,
-        visibility: D3D12_SHADER_VISIBILITY) -> D3D12_ROOT_PARAMETER1 {
-        let constants = D3D12_ROOT_CONSTANTS { 
-            ShaderRegister: shader_register, 
-            RegisterSpace: register_space,
-            Num32BitValues: num_32bit_values
-        };
-        return D3D12_ROOT_PARAMETER1 {
-            ParameterType: D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS,
-            Anonymous: D3D12_ROOT_PARAMETER1_0 {
-                Constants: constants
-            },
-            ShaderVisibility: visibility
-        };
-    }
-
-    fn init_1_1(params: Vec<D3D12_ROOT_PARAMETER1>,
-         samplers: Vec<D3D12_STATIC_SAMPLER_DESC>,
-          flags: D3D12_ROOT_SIGNATURE_FLAGS)
-           -> D3D12_VERSIONED_ROOT_SIGNATURE_DESC {
-        let desc = D3D12_ROOT_SIGNATURE_DESC1 {
-            NumParameters: params.len() as u32,
-            pParameters: params.as_ptr(),
-            NumStaticSamplers: samplers.len() as u32,
-            pStaticSamplers: samplers.as_ptr(),
-            Flags: flags
-        };
-
-        return D3D12_VERSIONED_ROOT_SIGNATURE_DESC {
-             Version: D3D_ROOT_SIGNATURE_VERSION_1_1, 
-             Anonymous: D3D12_VERSIONED_ROOT_SIGNATURE_DESC_0 { Desc_1_1: desc }
-            };
-    }
-
-    fn from_pixel_shader(pixel_shader: &PixelShader, device: &ID3D12Device2) -> D3DRootSignature {
-        let feature_data = Self::check_root_signature_features(device);
-    
-        let mut params:Vec<D3D12_ROOT_PARAMETER1> = Vec::new();
-        params.push(Self::init_as_constants(D3DSlowVSConstants::SIZE_32_BIT, 0, 0, D3D12_SHADER_VISIBILITY_VERTEX));
-        params.push(Self::init_as_constants(D3DFastConstants::SIZE_32_BIT, 1, 0, D3D12_SHADER_VISIBILITY_ALL));
-    
-        let inputs = pixel_shader.inputs;
-        if !inputs.is_empty()
-        {   
-            // uint32_t t = static_cast<uint32_t>(num_inputs);
-            // CD3DX12_DESCRIPTOR_RANGE1 desc_range{D3D12_DESCRIPTOR_RANGE_TYPE::D3D12_DESCRIPTOR_RANGE_TYPE_SRV, t, 0};
-            // params[2].InitAsDescriptorTable(1U, &desc_range, D3D12_SHADER_VISIBILITY_PIXEL);
-        }
-    
-        // create a static sampler
-        let sampler = D3D12_STATIC_SAMPLER_DESC{
-            Filter: D3D12_FILTER_MIN_MAG_MIP_POINT,
-            AddressU: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
-            AddressV: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
-            AddressW: D3D12_TEXTURE_ADDRESS_MODE_BORDER,
-            MipLODBias: 0.0,
-            MaxAnisotropy: 0,
-            ComparisonFunc: D3D12_COMPARISON_FUNC_NEVER,
-            BorderColor: D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK,
-            MinLOD: 0.0,
-            MaxLOD: D3D12_FLOAT32_MAX,
-            ShaderRegister: 0,
-            RegisterSpace: 0,
-            ShaderVisibility: D3D12_SHADER_VISIBILITY_PIXEL,
-        };
-    
-        let flags: D3D12_ROOT_SIGNATURE_FLAGS =
-            D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS;
-        
-
-        let desc = Self::init_1_1(params, vec![sampler], flags);
-        
-        let mut blob = None;
-        let mut error_blob = None;
-        unsafe {
-            D3D12SerializeVersionedRootSignature(&desc, &mut blob, &mut error_blob);
-        }
-        let root_signature = unsafe { device.CreateRootSignature(0, &blob) }.unwrap();
-    
-        return D3DRootSignature { root_signature: root_signature };
-    }
-}
-
 pub struct D3DRenderer<'a> {
     graphics: &'a D3DGraphics,
 
@@ -620,8 +494,8 @@ pub struct D3DRenderer<'a> {
 impl<'a> Renderer for D3DRenderer<'a> {
     fn apply(&mut self, items: RenderQueueItems) {
         for instance in items.new_meshes {
-            let d3d_mesh = self.graphics.load_mesh(instance.mesh);
-            let d3d_material = self.graphics.load_material(instance.material);
+            // let d3d_mesh = self.graphics.load_mesh(instance.mesh);
+            // let d3d_material = self.graphics.load_material(instance.material);
 
             let ps = instance.material.shader;
             let root_signature = D3DRootSignature::from_pixel_shader(ps, &self.graphics.device);
